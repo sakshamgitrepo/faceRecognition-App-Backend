b822ddbf24ddf85fd933e9fb4f3a37cec6305403
Saksham <sakshamraj61@gmail.com>
2022-10-16 06:08:53 +0530

    secure connection code

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+const Clarifai = require('clarifai');
 
+// Using Knex to connect server and database
 const db = knex({
   // connect to your own database here:
   client: "pg",
@@ -14,43 +16,50 @@ const db = knex({
     database: "smart_brain",
   },
 });
+
 const app = express();
 app.use(cors());
 const PORT = 3000;
 app.use(bodyParser.json());
 
+// ============================ root path routing of server ===================================
 app.get("/", (req, res) => {
   res.send("running");
 });
 
-// ==========================================SIGN IN==========================================
+// ========================================== SIGN IN ==========================================
 app.post("/signin", (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-		return res.status(400).json('incorrect form submission');
-	}
- db.select('email', 'hash').from('login')
- .where('email', '=', email)
- .then(data =>{
-  const isValid = bcrypt.compareSync(password, data[0].hash);
-  if (isValid) {
-    return db.select('*').from('users')
-      .where('email', '=', email)
-      .then(user => {
-        res.json(user[0])
-      }).catch(err => res.status(400).json('unable to get user'))
-    }else {
-      res.status(400).json('wrong credentials')
-    }
- }).catch(err => res.status(400).json('wrong credentials'))
+    return res.status(400).json("incorrect form submission");
+  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
 });
 
-// ============================================REGISTER=========================================
+// ============================================ REGISTER =========================================
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
   if (!email || !name || !password) {
-		return res.status(400).json('incorrect form submission');
-	}
+    return res.status(400).json("incorrect form submission");
+  }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
     trx
@@ -70,14 +79,14 @@ app.post("/register", (req, res) => {
           })
           .then((user) => {
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
       .catch(trx.rollback);
   }).catch((err) => res.status(400).json("unable to register"));
 });
 
-// =============================================PROFILE=========================================
+// ============================================= PROFILE =========================================
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
   db.select("*")
@@ -93,7 +102,7 @@ app.get("/profile/:id", (req, res) => {
     .catch((err) => res.status(404).json("Error getting user"));
 });
 
-//================================================IMAGE===========================================
+//================================================ IMAGE ===========================================
 app.put("/image", (req, res) => {
   const { id } = req.body;
   db("users")
@@ -106,6 +115,20 @@ app.put("/image", (req, res) => {
     .catch((err) => res.status(400).json("unable to get entries"));
 });
 
+// ============================================= Image Clarifai ====================================
+app.post("/imageurl", (req, res) => {
+  const app = new Clarifai.App({
+    apiKey: "7fde0333fa9b419189004df2254258b4", 
+  });
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+});
+
+//====================================Port======================================== 
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
 });
